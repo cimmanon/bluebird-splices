295bd5f6e9f0d69d3545e830e5f876eed6dbf18a
cimmanon <chris@cimmanon.org>
2017-03-31 15:44:15 -0400

    Added prettyNumberSplice for comma delimited numbers

--- Heist/Splices/Camellia/Types.hs
+++ Heist/Splices/Camellia/Types.hs
@@ -6,6 +6,9 @@ module Heist.Splices.Camellia.Types where
 
 import Data.Map.Syntax
 import Data.Text (Text)
+import Data.List (intercalate)
+import Data.List.Split (chunksOf)
+import Data.Monoid ((<>))
 import qualified Data.Text as T
 import Heist.Interpreted
 
@@ -37,3 +40,12 @@ dateFormatSplice locale defaultFormat t = do
 	let
 		format = maybe defaultFormat T.unpack $ X.getAttribute "format" node
 	textSplice $ T.pack $ formatTime locale format t
+
+-- this converts a number to a comma delimited number
+prettyNumberSplice :: (Monad m, Num n, Show n) => n -> Splice m
+prettyNumberSplice i =
+	let
+		(whole, decimal) = break (== '.') $ show i
+		commaWhole = reverse $ intercalate "," $ chunksOf 3 $ reverse whole
+		dotHalf = if not (null decimal) then '.' : decimal else []
+	in stringSplice $ commaWhole <> dotHalf
