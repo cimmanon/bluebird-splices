9609821de2e34b7268dff48eebf1990ad2d7456c
cimmanon <chris@cimmanon.org>
2018-01-12 11:48:02 -0400

    Refactored some of the session splices and added a few new helper functions

--- Heist/Splices/Camellia/Session.hs
+++ Heist/Splices/Camellia/Session.hs
@@ -4,26 +4,51 @@ module Heist.Splices.Camellia.Session where
 
 ----------------------------------------------------------------------
 
+import Control.Monad ((<=<))
 import Data.Maybe (isJust)
 import qualified Data.Text as T
 import Heist.Interpreted
+import Safe (readMay)
 
 -- for Session stuff
 import Control.Monad.Trans.Class (MonadTrans, lift)
-import Snap.Core
 import Snap.Snaplet (SnapletLens, Handler, withTop)
 import Snap.Snaplet.Session (SessionManager, getFromSession)
 import Snap.Snaplet.Heist (SnapletISplice)
 
-import Heist.Splices.Camellia.Visibility
+import Heist.Splices.Camellia.Visibility (maybeSplice, ifSplice)
 
-----------------------------------------------------------------------
+{----------------------------------------------------------------------------------------------------{
+                                                                      | Helpers
+}----------------------------------------------------------------------------------------------------}
 
+readFromSession :: Read a => T.Text -> Handler b SessionManager (Maybe a)
+readFromSession = fmap (readMay <=< fmap T.unpack) . getFromSession
+
+inSession :: T.Text -> Handler b SessionManager Bool
+inSession = fmap isJust . getFromSession
+
+liftFromSession :: MonadTrans t => SnapletLens b SessionManager -> T.Text -> t (Handler b v) (Maybe T.Text)
+liftFromSession sess = lift . withTop sess . getFromSession
+
+{----------------------------------------------------------------------------------------------------{
+                                                                      | Splices
+}----------------------------------------------------------------------------------------------------}
+
+sessionTextSplice :: SnapletLens b SessionManager -> T.Text -> SnapletISplice b
+sessionTextSplice sess key = maybeSplice textSplice =<< liftFromSession sess key
+
+sessionIfSplice :: SnapletLens b SessionManager -> T.Text -> SnapletISplice b
+sessionIfSplice sess key = ifSplice =<< lift (withTop sess $ inSession key)
+
+{-# DEPRECATED getFromSession' "Deprecated in favor of liftFromSession" #-}
 getFromSession' :: MonadTrans t => SnapletLens b SessionManager -> T.Text -> t (Handler b v) (Maybe T.Text)
-getFromSession' sess = lift . withTop sess . getFromSession
+getFromSession' = liftFromSession
 
+{-# DEPRECATED sessionInfoSplice "Deprecated in favor of sessionTextSplice" #-}
 sessionInfoSplice :: SnapletLens b SessionManager -> T.Text -> SnapletISplice b
-sessionInfoSplice sess key = maybeSplice textSplice =<< getFromSession' sess key
+sessionInfoSplice = sessionTextSplice
 
+{-# DEPRECATED sessionHasSplice "Deprecated in favor of sessionIfSplice" #-}
 sessionHasSplice :: SnapletLens b SessionManager -> T.Text -> SnapletISplice b
-sessionHasSplice sess key = ifSplice' . isJust =<< getFromSession' sess key
+sessionHasSplice = sessionIfSplice
